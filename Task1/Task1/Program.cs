5d16286f5c10cdb36f7b5eac36fd8d90d4b84c0b
GuziyV <volodyaguziy3@gmail.com>
2018-09-18 21:30:07 +0300

    write errors from file

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Task1
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Task1
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Task1.Models;
+
     internal class Program
     {
         private static void Main(string[] args)
         {
+            List<HttpError> errors = HttpError.GetHttpErrorsFromFile(@"..\..\Files\file1.txt").ToList();
+
+            foreach (var err in errors)
+            {
+                Console.WriteLine(err.ErrorCode);
+            }
         }
     }
 }
