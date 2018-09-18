5d798508e75aee451186ece72aa00c15aabc1c27
GuziyV <volodyaguziy3@gmail.com>
2018-09-18 20:56:27 +0300

    Create HttpError class

---
+++ Task1/Task1/Models/HttpError.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Models
+{
+    class HttpError : IComparable<HttpError>
+    {
+        public int ErrorCode { get; set; }
+
+        public string Description { get; set; }
+
+        public int CompareTo(HttpError obj)
+        {
+            return this.ErrorCode.CompareTo(obj.ErrorCode);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as HttpError;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.ErrorCode.Equals(item.ErrorCode);
+        }
+    }
+}
