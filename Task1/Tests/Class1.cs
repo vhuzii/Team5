e1d644d15c6229c00a81fb9868e912a8ae305cfd
GuziyV <volodyaguziy3@gmail.com>
2018-09-27 20:14:15 +0300

    Add projects with tests

---
+++ Task1/Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class TestLogging
+        {
+            [Test]
+            public void Add()
+            {
+        
+                Assert.That(3, Is.EqualTo(3));
+            }
+        }
+    }
+}
