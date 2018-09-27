4d955f6552e393f99e41d435bb2821f37d95cbbc
GuziyV <volodyaguziy3@gmail.com>
2018-09-27 16:05:13 +0300

    Add IRepository and figure repository

---
+++ Task2/Data Access/Repositories/FigureRepository.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="FigureRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Data_Access.Repositories
+{
+    using System.Collections.Generic;
+    using Data_Access.Repositories.Interfaces;
+    using Task2.Models.Abstract;
+
+    /// <summary>
+    /// Repository to store figures
+    /// </summary>
+    public class FigureRepository : IRepository<Figure>
+    {
+        private readonly List<Figure> figures = new List<Figure>();
+
+        /// <inheritdoc/>
+        public void Add(Figure entity)
+        {
+            this.figures.Add(entity);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Figure> GetAll()
+        {
+            return this.figures;
+        }
+
+        /// <inheritdoc/>
+        public void Remove(Figure entity)
+        {
+            this.figures.Remove(entity);
+        }
+    }
+}
---
+++ Task2/Data Access/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="IRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Data_Access.Repositories.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Task2.Models.Abstract;
+
+    /// <summary>
+    /// interface of repository
+    /// </summary>
+    /// <typeparam name="T">Stored entity</typeparam>
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// Method to add entity
+        /// </summary>
+        /// <param name="entity">entity to add</param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Method to remove entity
+        /// </summary>
+        /// <param name="entity">entity to remove</param>
+        void Remove(T entity);
+
+        /// <summary>
+        /// get all elements from repository
+        /// </summary>
+        /// <returns>returns all elements from repository</returns>
+        IEnumerable<T> GetAll();
+    }
+}
