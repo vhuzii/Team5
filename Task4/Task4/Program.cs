fdda8173afbe2ded615fd1d8f6f3329b13706f96
antooa <antooa@yandex.ru>
2018-12-26 10:34:23 +0200

    Improved query execution

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -4,6 +4,7 @@
 
 namespace Task4
 {
+    using System.Collections.Generic;
     using System.Configuration;
     using System.Data.SqlClient;
 
@@ -14,10 +15,19 @@ namespace Task4
     {
         private static void Main(string[] args)
         {
-            string sql = "Select FirstName, LastName From Employees Where FirstName Like 'A%'";
+            List<string> sqls = new List<string>();
+            sqls.Add("Select FirstName, LastName, BirthDate From Employees Where Month(GetDate()) = Month(BirthDate)");
+            sqls.Add("Select Distinct FirstName, LastName From Employees, Orders Where ShipCity = 'Madrid' And Orders.EmployeeID = Employees.EmployeeID");
+            sqls.Add("Select Count(o.OrderID) From Orders As o, Customers As c Where o.CustomerID = c.CustomerID And c.Country = 'France' Group By c.CustomerID");
+            sqls.Add("Select Top 5 * From Employees Order By EmployeeID DESC");
+            sqls.Add("Select e.FirstName, e.LastName, Count(o.OrderID) From Employees As e, Orders As o Where e.EmployeeID = o.EmployeeID And " +
+            "OrderDate Between '01-Jan-97 00:00:00 AM' And '31-Dec-97 12:00:00 PM' Group By e.FirstName, e.LastName, e.EmployeeID");
+            sqls.Add("Select c.ContactName From Orders As o, Customers As c Where o.CustomerID = c.CustomerID And c.Country = 'France' " +
+            "Group By c.CustomerID, c.ContactName Having Count(o.OrderID) > 1");
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind";
             DatabaseService service = new DatabaseService(connectionString);
-            service.ShowData(sql);
+            service.ShowData(sqls[1]);
+            System.Console.ReadLine();
         }
 
     }
